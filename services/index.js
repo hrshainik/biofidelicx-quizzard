4ae103102304c6d6dde6c9849d18961a818eb4e0
Habibur Rahman <hrsshainik@gmail.com>
2022-11-12 08:01:30 +0600

    add api token

--- services/index.js
+++ services/index.js
@@ -9,21 +9,45 @@ const QUIZ_URLS = {
 // };
 
 export const getQuizzes = async () => {
-  const res = await fetch(`${QUIZ_URLS.quiz}?populate=deep,2`);
+  const res = await fetch(`${QUIZ_URLS.quiz}?populate=deep,2`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+    },
+  });
   return await res.json();
 };
 
 export const getQuiz = async (id) => {
-  const res = await fetch(`${QUIZ_URLS.quiz}/${id}?populate=deep,3`);
+  const res = await fetch(`${QUIZ_URLS.quiz}/${id}?populate=deep,3`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+    },
+  });
   return await res.json();
 };
 
 export const getCategories = async () => {
-  const res = await fetch(`${QUIZ_URLS.category}?populate[0]=quizzes`);
+  const res = await fetch(`${QUIZ_URLS.category}?populate[0]=quizzes`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+    },
+  });
   return await res.json();
 };
 
 export const getCategory = async (id) => {
-  const res = await fetch(`${QUIZ_URLS.category}/${id}?populate=deep,3`);
+  const res = await fetch(`${QUIZ_URLS.category}/${id}?populate=deep,3`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+    },
+  });
   return await res.json();
 };
